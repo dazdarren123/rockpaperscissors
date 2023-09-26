65f7b7bda43478129a0048b8ec601710d32c9545
Darren <werulefoursquare@gmail.com>
2023-09-26 16:36:13 -0400

    add get user choice

--- javascript.js
+++ javascript.js
@@ -1,9 +1,8 @@
 // creating rock paper scissors involves letting user type in a move and then comparing that move to a randomly generated move by a computer
 
 
-//to get the user input, we need to prompt the user for a case insenitive move of rock, paper, or scissors, assign that to a variable. we also have to have error handling in case the user types in an incorrect move
 
-
+// ****** COMPUTER SELECTION SECTION BEGIN *****
 //to get a randomly generated move from a computer, we need to create a function that selects a number randomly from 1 to three
 function randomNumber (){
     //this returns a random integer between 1-3
@@ -21,10 +20,23 @@ function getComputerChoice (){
   } else {
     computerSelection = "Scissors";
   }
-  
   console.log(computerSelection);
 }
 getComputerChoice();
+// ****** COMPUTER SELECTION SECTION END *****
+
+// ****** PLAYER SELECTION SECTION BEGIN *****
+//to get the user input, we need to prompt the user for a case insenitive move of rock, paper, or scissors, assign that to a variable. we also have to have error handling in case the user types in an incorrect move. we also need to make sure that the case is solved
+function getPlayerChoice(){
+    let playerSelection = prompt("Please enter a move","Rock, Paper, or Scissors.");
+    playerSelection = playerSelection.toLowerCase();
+    playerSelection = playerSelection.charAt(0).toUpperCase()+playerSelection.slice(1);
+    console.log(playerSelection);
+}
+
+getPlayerChoice();
+
 //once we have the user input and the computers move, we can compare the moves by using if, then loops
-//we will then ouput feedback based on who's move wins, who's move loses, and if there is a draw
+//we will then print a statement based on who's move wins, who's move loses, and if there is a draw, utilizing the variables associated with each move, concatenating with the result
+
 
